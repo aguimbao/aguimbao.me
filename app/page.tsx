b4c323d5948fe70aaa280c9f21b47f43cbcf49f7
Abraham <abraham@aguimbao.me>
2026-01-28 01:49:41 -0500

    Fix line break in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -486,8 +486,8 @@ export default function Home() {
                   </p>
                   <p className='max-w-4xl text-center text-base leading-relaxed tracking-normal text-muted-foreground sm:text-lg'>
                     I have years of experience interacting with different types of software and infrastructure in diverse environments and teams, and I love learning new things, architecturing useful tools, and tinkering with new technologies.
-                    <br>
                   </p>
+                  <br>
                 </div>
               </div>
               <div className='mb-4 sm:mb-6'>
